f89bbe6677b6df9769eeb878704c9d28c0c85307
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-18 10:49:46 +0200

    add prompt add innerhtml

--- js/app.js
+++ js/app.js
@@ -10,11 +10,13 @@ const numbersDomElement = document.getElementById('numeri-random');
 //recupero timer dal DOM
 const timerOutDomElement = document.getElementById('timer');
 //recupero timeout dal DOM
-const timeOutDomeElement = document.getElementById('timeout');
+const timeOutDomElement = document.getElementById('timeout');
+//recupero numeri-ricordati dal DOM
+const numeriRicordatiDomElement = document.getElementById('numeri-ricordati');
 
 //aggiungo eventlistener, al click del bottone inizia la funzione
 playButtonDomElement.addEventListener('click', function () {
-
+  numeriRicordatiDomElement.innerHTML = '';
   function generateRandomNumbers() {
     //creo l'Array vuoto
     const randomNumbers = [];
@@ -34,39 +36,61 @@ playButtonDomElement.addEventListener('click', function () {
   //recupero l'elemento dal DOM per far stampare i 5 numeri random in pagina
   numbersDomElement.innerHTML = randomNumbersArray
 
-  //stampare countdown in pagina
+
+  // Stampare countdown in pagina
   let countdown = 5;
   timerOutDomElement.innerHTML = 'Countdown: ' + countdown;
   //creo intervallo per il countdown
   const countdownInterval = setInterval(function () {
     countdown--;
-    //visualizza il countdown aggiornato nella pagina
+    //visualizza countdown in pagina
     timerOutDomElement.innerHTML = 'Countdown: ' + countdown;
     //condizionali
-    //se il countdown è minore o uguale di 0
+    //se il timer è minore o uguale a zero
     if (countdown <= 0) {
-      //allora cancella il countdown
+      //allora cancella timer
       clearInterval(countdownInterval);
-      //cancella numeri casuali dalla pagina
+      //far sparire i numeri dell array
       numbersDomElement.innerHTML = '';
-      //cancella il countdown dalla pagina
+      //far sparire il timer dalla pagina
       timerOutDomElement.innerHTML = '';
+
+      // Impostare timer 30 secondi prima di chiedere i numeri all'utente
+      let timerNumber = 30;
+      //visualizzare countdown in pagina
+      timeOutDomElement.innerHTML = 'Countdown: ' + timerNumber;
+      //settare intervallo 
+      const timerCountdown = setInterval(function () {
+        timerNumber--;
+        //visualizza countdown in pagina
+        timeOutDomElement.innerHTML = 'Countdown: ' + timerNumber;
+        //condizionali
+        //se il timer è minore o uguale a zero
+        if (timerNumber <= 0) {
+          //allora cancella timer
+          clearInterval(timerCountdown);
+          //far sparire il timer dalla pagina
+          timeOutDomElement.innerHTML = '';
+
+          // Array per memorizzare i numeri che l'utente ricorda
+          const userNumbers = [];
+
+          for (let i = 0; i < 5; i++) {
+            const userInput = prompt('Inserisci il numero che ricordi');
+            // Confronto se l'input dell'utente è presente nell'array di numeri casuali
+            if (randomNumbersArray.includes(parseInt(userInput)) && !userNumbers.includes(parseInt(userInput))) {
+              userNumbers.push(parseInt(userInput));
+            }
+          }
+
+          // Stampo i numeri che l'utente ha individuato
+          console.log('Numeri individuati dal utente:', userNumbers);
+
+          //stampare in pagina i numeri 
+         numeriRicordatiDomElement.innerHTML = 'lista numeri ricordati ' + userNumbers;
+         numeriRicordatiDomElement.innerHTML += '<br> quanti numeri hai ricordato ' + userNumbers.length;
+        }
+      }, 1000);
     }
   }, 1000);
-
-  //stampare countdown in pagina
-  //impostare timer 30 secondi prima di chiedere i numeri all'utente
-  let timerNumber = 30;
-  timeOutDomeElement.innerHTML = 'Countdown: ' + timerNumber;
-
-  let idInterval = setInterval(function timeOut() {
-    if (timerNumber === 0) {
-      clearInterval(idInterval);
-      timeOutDomeElement.innerHTML = '';
-    } else {
-      timerNumber--
-      timeOutDomeElement.innerHTML = 'Countdown: ' + timerNumber;
-    }
-  }, 1000)
-})
-
+});
